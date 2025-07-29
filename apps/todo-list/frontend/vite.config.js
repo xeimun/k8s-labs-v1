4c5b41b88c62612008144d971539f6569993f530
yun\user <captain-yun7@github.com>
2025-07-29 22:40:00 +0900

    fix: apps/todo-list/frontend vite 환경변수 설정 방법 변경

--- apps/todo-list/frontend/vite.config.js
+++ apps/todo-list/frontend/vite.config.js
@@ -1,21 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // 모든 인터페이스에 바인딩
-    port: 3000, // 환경 변수를 통해 포트를 설정
+    host: '0.0.0.0',
+    port: 3000,
     proxy: {
-      // '/api'로 시작하는 요청을 백엔드로 프록시
       '/api': {
-        target: `${import.meta.env}/api`, // Spring Boot 백엔드 컨테이너 주소
+        target: `${process.env.VITE_API_URL}`, // 환경 변수 사용
         changeOrigin: true,
       },
-      // '/auth'로 시작하는 요청도 백엔드로 프록시
       '/auth': {
-        target: `${import.meta.env}/api`, // Spring Boot 백엔드 컨테이너 주소
+        target: `${process.env.VITE_API_URL}`, // 동일한 백엔드 주소 사용
         changeOrigin: true,
       },
     },
